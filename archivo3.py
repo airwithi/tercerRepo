0fcf598cd1c3df2071d2ae7417d2427b76bd7acb
Esteban F <esteban.farfan.quevedo@gmail.com>
2022-08-01 10:50:06 -0500

    modificacion despues de restauracion a segundo commit

--- archivo3.py
+++ archivo3.py
@@ -2,4 +2,4 @@ print("hola este es un archivo para el tercer repositorio")
 
 print("esta impresion corresponde a una modificacion para un segundo commit")
 
-print("Esta impresion corresponde a una modificacion para un tercer commit")
+print("esta impresion elimita el tercer commit y reescribe esta linea en caso de correccion d eun error")
