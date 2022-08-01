67a99458403bae6ad5b6b4ff3b3858ea82e51899
airwithi <110253813+airwithi@users.noreply.github.com>
2022-08-01 11:50:53 -0500

    Update archivo3.py

--- archivo3.py
+++ archivo3.py
@@ -3,3 +3,5 @@ print("hola este es un archivo para el tercer repositorio")
 print("esta impresion corresponde a una modificacion para un segundo commit")
 
 print("esta impresion elimita el tercer commit y reescribe esta linea en caso de correccion d eun error")
+
+print("Esta impresion es para un commit realizado desde github")
