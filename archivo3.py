62363f607663ce8efde3932f5ec1267a8ff483c9
Esteban F <esteban.farfan.quevedo@gmail.com>
2022-08-01 10:38:10 -0500

    modificando archivo para segundo commit

--- archivo3.py
+++ archivo3.py
@@ -1,1 +1,5 @@
 print("hola este es un archivo para el tercer repositorio")
+
+print("esta impresion corresponde a una modificacion para un segundo commit")
+
+print("Esta impresion corresponde a una modificacion para un tercer commit")
