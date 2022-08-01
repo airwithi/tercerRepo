37a02ea94fa5a7290624170cbebeb5636a740f85
Esteban F <esteban.farfan.quevedo@gmail.com>
2022-08-01 12:47:39 -0500

    edicion de archivo por medio de un repositorio clonado

--- archivo3.py
+++ archivo3.py
@@ -5,3 +5,5 @@ print("esta impresion corresponde a una modificacion para un segundo commit")
 print("esta impresion elimita el tercer commit y reescribe esta linea en caso de correccion d eun error")
 
 print("Esta impresion es para un commit realizado desde github")
+
+print("a partir de un repo clonado, este es un intento de hacer commit")
